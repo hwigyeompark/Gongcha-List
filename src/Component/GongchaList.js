9bb229c583e3cb213decc4fbcaa6542b89b69c1e
hwigyeompark <phg5590@gmail.com>
2018-05-15 21:19:19 +0900

    update GongchaList

--- src/Component/GongchaList.js
+++ src/Component/GongchaList.js
@@ -7,6 +7,29 @@ class GongchaList extends Component{
       pic : 'No Pic'
     };
     render(){
+        const styles = {
+            container : {
+                border : '1px solid #555',
+                borderRadius : '20px',
+                width : '30%',
+                height : '300px',
+                margin : '0 auto',
+                padding : '50px',
+            },
+            picStyle : {
+                width:'250px',
+                height: '250px',
+                backgroundImage : `url(${this.props.pic})`,
+                backgroundSize : 'contain',
+                backgroundRepeat : 'no-repeat',
+                borderRadius:'50%'
+            },
+            align : {
+                fontSize : '1.2em',
+                display : 'inline-block',
+                verticalAlign : 'middle'
+            }
+        };
         return(
           <div></div>
         );
