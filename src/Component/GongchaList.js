2beb1df33edf4b7722db934b060d17ff6889fea7
hwigyeompark <phg5590@gmail.com>
2018-05-15 21:15:20 +0900

    update GongchaList

--- src/Component/GongchaList.js
+++ src/Component/GongchaList.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
 
 class GongchaList extends Component{
+    static defaultProps = {
+      name : 'No Name',
+      price : 'No Price',
+      pic : 'No Pic'
+    };
     render(){
         return(
           <div></div>
